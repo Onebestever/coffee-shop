a0f57588d86facb421486906e07e53ef61374ea9
Onebestever <carla.h1248@gmail.com>
2022-06-06 09:27:08 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://shay:SHCBk99XpMHfBkqX@cluster0.2n0wm.mongodb.net/<database here>?retryWrites=true&w=majority',
+    'dbName': '<database here>'
+};
